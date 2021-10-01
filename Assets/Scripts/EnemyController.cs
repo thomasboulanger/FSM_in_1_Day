f4bc247b90cd55bcb7549c97ac2037a402e2d182
thomasboulanger <60351261+thomasboulanger@users.noreply.github.com>
2021-10-01 10:45:51 +0200

    init

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public GameObject angleOfView;
+
+    private NavMeshAgent _navMeshAgent;
+    private bool _hasSpottedSomeone;
+
+    enum enemyState
+    {
+        idle,
+        chasing,
+        search
+    }
+    private enemyState currentState = enemyState.idle;
+    
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _hasSpottedSomeone = false;
+    }
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case enemyState.idle:
+                Idle();
+                if (angleOfView)
+                {
+                    
+                }
+                break;
+            
+            case enemyState.chasing:
+                Chasing();
+                break;
+            
+            case enemyState.search:
+                Search();
+                break;
+            
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        _navMeshAgent.destination = GameObject.Find("Player").transform.position;
+    }
+
+    private void Idle()
+    {
+        
+    }
+    
+    private void Chasing()
+    {
+        
+    }
+
+    private void Search()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            _hasSpottedSomeone = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    
+    private Rigidbody _rb;
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis ("Horizontal");
+        float vertical = Input.GetAxis ("Vertical");
+        _rb.velocity = new Vector3(horizontal * speed, _rb.velocity.y, vertical * speed);
+    }
+}
